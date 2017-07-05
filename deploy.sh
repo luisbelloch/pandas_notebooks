7af744e2cc0cbe1638bab6eafa9154ac32ffc944
Luis Belloch <luis@luisbelloch.es>
2017-07-06 00:12:52 +0200

    Live reload

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eoi pipefail
+echo -e "\033[0;36mRELOAD\033[0m $1 $(date +"%H%m%S")"
+jupyter nbconvert live.ipynb --to html --output-dir=$(pwd)/live
+scp live/live.html root@live.luisbelloch.es:/var/www/html/index.html
+
