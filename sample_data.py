f53f9007d3d6f4ecfff2dfaa8eb846c0aca9f75a
Luis Belloch <luis@luisbelloch.es>
2020-07-01 23:18:44 +0200

    Fix plotting notebook after pandas update

--- sample_data.py
+++ sample_data.py
@@ -1,11 +1,13 @@
 import pandas as pd
-prices = pd.read_msgpack('./data/prices.mp')
+
+prices = pd.read_csv('./data/prices.csv', index_col=[0,1], parse_dates=True)
 
 def stock_prices(symbol):
   data = prices[prices.index.get_level_values('Symbol').isin([symbol])]
-  data.index = data.index.droplevel(0)
-  return data
+  return prices.loc[symbol]
 
 # https://docs.python.org/3/library/warnings.html#warning-filter
 import warnings
 warnings.filterwarnings(action='once')
+
+# pd.set_option('mode.chained_assignment', None)
