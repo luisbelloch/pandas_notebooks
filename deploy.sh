ef069e76b3d57de317a265f73aef2a16348c1929
Luis Belloch <luis@luisbelloch.es>
2020-07-02 22:18:23 +0200

    Adjusted live.sh to work with GCP

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# debian-s-1vcpu-1gb-ams3-01 Debian 9.4 x64
-set -eou pipefail
-echo -e "\033[0;36mRELOAD\033[0m $1 $(date +"%H%m%S")"
-jupyter nbconvert live.ipynb --to html --output-dir=$(pwd)/live
-scp live/live.html root@live.luisbelloch.es:/var/www/html/index.html
--- live.sh
+++ live.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -euo pipefail
-mkdir -p live
-rm -rf live/*
-fswatch -0 -or live.ipynb | xargs -0 -n 1 ./deploy.sh
+
+readonly OUTPUT=live
+
+mkdir -p "${OUTPUT}"
+rm -rf "${OUTPUT}"/*
+
+fswatch live.ipynb | while read -r fpath; do \
+  echo -e "\033[0;36mRELOAD\033[0m $fpath $(date +"%H%m%S")"
+  jupyter nbconvert live.ipynb --to html --output-dir="${OUTPUT}"
+  gsutil -h "Cache-Control:no-store,max-age=0" \
+    cp "${OUTPUT}/live.html" gs://bigdata.luisbelloch.es/en_directo.html
+done
 
