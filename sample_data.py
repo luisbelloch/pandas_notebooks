054320ee3be7821d6a878a6ca1131a8471443b85
Luis Belloch <luis@luisbelloch.es>
2021-06-29 21:42:07 +0200

    Remove warning for chained assignment

--- sample_data.py
+++ sample_data.py
@@ -4,10 +4,9 @@ prices = pd.read_csv('./data/prices.csv', index_col=[0,1], parse_dates=True)
 
 def stock_prices(symbol):
   data = prices[prices.index.get_level_values('Symbol').isin([symbol])]
-  return prices.loc[symbol]
+  return prices.loc[symbol].copy()
 
 # https://docs.python.org/3/library/warnings.html#warning-filter
-import warnings
-warnings.filterwarnings(action='once')
-
+# import warnings
+# warnings.simplefilter(action='ignore')
 # pd.set_option('mode.chained_assignment', None)
