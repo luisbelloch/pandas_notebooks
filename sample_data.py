bb9c70d90226de71e7121c3a6762d5417f9bfe56
Luis Belloch <luis@luisbelloch.es>
2022-07-02 09:42:50 +0200

    Updated getting started notebook

--- sample_data.py
+++ sample_data.py
@@ -3,7 +3,6 @@ import pandas as pd
 prices = pd.read_csv('./data/prices.csv', index_col=[0,1], parse_dates=True)
 
 def stock_prices(symbol):
-  data = prices[prices.index.get_level_values('Symbol').isin([symbol])]
   return prices.loc[symbol].copy()
 
 # https://docs.python.org/3/library/warnings.html#warning-filter
